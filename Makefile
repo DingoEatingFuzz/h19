428c0b643f1e9e03d947c36599a4735f2181e9a7
Michael Lange <dingoeatingfuzz@gmail.com>
2019-08-16 23:56:59 -0700

    Start automating the start up process

---
+++ Makefile
@@ -0,0 +1,5 @@
+axidraw:
+	cd services/axidraw && npm start
+
+ngrok:
+	ngrok http 8080
---
+++ start.sh
@@ -0,0 +1,11 @@
+echo "Configuring environment..."
+export NOMAD_ADDR=hashi.plot.technology:4646
+export CONSUL_HOST=hashi.plot.technology:8500
+
+echo "Setting consul key/values..."
+consul kv put axidraw_address $1
+consul kv put current_product vagrant
+
+echo "Starting nomad jobs..."
+nomad run jobs/fabio.hcl
+nomad run jobs/webhooks.hcl
