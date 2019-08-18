08227ae65a626b93056141dfb1eaa3dfdcaf10fb
Michael Lange <dingoeatingfuzz@gmail.com>
2019-08-18 14:44:52 -0700

    Ignore layers and filtering

--- services/axidraw/src/saxi/replan.ts
+++ services/axidraw/src/saxi/replan.ts
@@ -16,7 +16,7 @@ export default function replan(inPaths: Vec2[][], planOptions: PlanOptions): Pla
   // Rescaling loses the stroke info, so refer back to the original paths to
   // filter based on the stroke. Rescaling doesn't change the number or order
   // of the paths.
-  paths = paths.filter((path, i) => planOptions.selectedLayers.has((inPaths[i] as any).stroke));
+  // paths = paths.filter((path, i) => planOptions.selectedLayers.has((inPaths[i] as any).stroke));
 
   if (planOptions.pointJoinRadius > 0) {
     paths = paths.map((p) => dedupPoints(p, planOptions.pointJoinRadius));
