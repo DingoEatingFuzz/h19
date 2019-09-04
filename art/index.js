9acf3598d842b27f4cda17377203ba1fc2ce5754
Michael Lange <dingoeatingfuzz@gmail.com>
2019-09-04 01:51:28 -0700

    Position Vault

--- art/index.js
+++ art/index.js
@@ -33,7 +33,7 @@ const positions = {
   vagrant: pos(2, -1.4, 1.6, -Math.PI / 4),
   packer: pos(1.33, -1.5, 1.5, -Math.PI / 4),
   terraform: pos(1.8, -1.5, 0.8, -0.7),
-  vault: pos(2, -1.8, 0.8, -Math.PI / 4),
+  vault: pos(1.9, -1.3, 1.5, -1.2),
   consul: pos(2, -1.8, 0.8, -Math.PI / 4),
   nomad: pos(1.8, -1.3, 1.3, -0.6)
 };
